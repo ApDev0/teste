992b44761ee890af45738a26c2a72497a5d4ac71
Anderson Pedro <113034804+ApDev0@users.noreply.github.com>
2023-11-27 03:36:21 +0000

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import '../styles/globals.scss'
 import Layout from "@/components/layouts";
 import React from "react";
-export default function ({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
     return (
         <Layout>
             <Component {...pageProps} />
