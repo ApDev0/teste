cf30e5af55f67059a5f1bbfe7aef4bd4e39e19b0
Anderson Pedro <113034804+ApDev0@users.noreply.github.com>
2023-12-13 11:11:50 +0000

    atualização Dezembro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
 import { useEffect, useState } from 'react'
 import styles from '../styles/home.module.scss'
 import Head from 'next/head'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { EffectCards, Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { EffectCards, EffectFade, Pagination, Navigation } from 'swiper/modules';
+
+
+import { FaPassport } from "react-icons/fa";
+import { HiOutlineIdentification } from "react-icons/hi2";
+import { GiAirplaneDeparture } from "react-icons/gi";
+import { CiSearch } from "react-icons/ci";
+
 import Link from 'next/link'
 import Image from 'next/image'
 
 import comunImage from '../../public/comuncados/isabel.jpg'
 import { Button } from '@/components/ui/Button'
+import Layout from '../components/layouts/index';
+
 export default function Home() {
-  const [sliderPerView, setSliderPerView] = useState(1)
+  const [swiperEffect, setSwiperEffect] = useState('fade');
+  const [slidesPerView, setSlidesPerView] = useState(4);
+
+
   const data = [
     { id: '1', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/08/fullstack-blog.png', titulo: 'Um bom programador mantem a disciplina' },
     { id: '2', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/08/home.png', titulo: 'As reuniões são igualmente importantes' },
@@ -17,20 +30,35 @@ export default function Home() {
     { id: '4', image: 'https://sujeitoprogramador.com/wp-content/uploads/2022/01/thumb-1.png', titulo: 'Ter um portfólio atrativo atrai novas oportunidades' }
   ]
 
-  useEffect(() => {
 
-    function handleResize(){
-      if(window.innerWidth < 720){
-        setSliderPerView(1)
-      }if(window.innerWidth > 720 && window.innerWidth <= 960){
-        setSliderPerView(3)
-      }else{
-        setSliderPerView(4)
+  useEffect(() => {
+    // Verificar o tamanho da tela e definir o efeito do Swiper
+    function handleResize() {
+      const screenWidth = window.innerWidth;
+
+      if (screenWidth < 768) {
+        setSwiperEffect('cards');
+        setSlidesPerView(1);
+      } else {
+        setSwiperEffect('fade');
+        setSlidesPerView(4);
       }
+
     }
 
+
+    // Adicionar um ouvinte de redimensionamento
+    window.addEventListener('resize', handleResize);
+
+    // Executar a verificação inicial
     handleResize();
-  })
+
+    // Remover o ouvinte de redimensionamento ao desmontar o componente
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
 
 
   return (
@@ -39,25 +67,22 @@ export default function Home() {
         <title>Consulado de Angola no Porto | Inicio</title>
       </Head>
       <div className={styles.Banner}>
-
         <Swiper
           slidesPerView={1}
           pagination={false}
           navigation={false}
           autoplay={false}
-          style={{
-            height: '100%',
-
-          }}
+          className={styles.sliderContent}
         >
           {data.map((item) => (
-            <SwiperSlide key={item.id}>
+            <SwiperSlide key={item.id} >
               <Image
                 src={item.image}
                 alt='slider'
-                layout="responsive"
-                width={350}
-                height={350}
+                width={80}
+                height={80}
+                layout='responsive'
+                priority={true}
                 rel='preload'
                 className={styles.slide_item}
               />
@@ -71,177 +96,173 @@ export default function Home() {
           }
         </Swiper>
       </div>
+      <div className={styles.wonerServices}>
+        <ul className={styles.ServiceList}>
+          <li>
+            <div className={styles.Icon}>
+              <FaPassport size={24} className={styles.svg} />
+            </div>
+            <Link href={'/'}>  Passaporte no estrangeiro</Link>
+          </li>
+          <li>
+            <div className={styles.Icon}>
+              <HiOutlineIdentification size={24} className={styles.svg} />
+            </div>
+            <Link href={'/'}>Bilhete de Identidade no Estrangeiro</Link>
+          </li>
+          <li>
+            <div className={styles.Icon}>
+              <GiAirplaneDeparture size={24} className={styles.svg} />
+            </div>
+            <Link href={'/'}>Alertas de Viagens</Link>
+          </li>
+          <li>
+            <div className={styles.Icon}>
+              <CiSearch size={24} className={styles.svg} />
+            </div>
+            <Link href={'/'}>Consultar estado do pedido</Link>
+          </li>
+        </ul>
+      </div>
 
-      <section className={styles.Comunicados}>
+      <section className={styles.Noticias}>
         <h3>Noticias</h3>
         <hr className={styles._lines} />
-        <Swiper
-          modules={[EffectCards, Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={250}
-          slidesPerView={4}
-          navigation={false}
-          pagination={false}
-          autoplay={{ delay: 6000 }}
-          style={{
-            marginTop: "2rem",
-          }}
-        >
-          <SwiperSlide>
-            <div className={styles._CardContent}>
-              <div className={styles.CardImage}>
-                <Image src={comunImage} alt='Imagem de Isabel dos santos' />
-              </div>
-              <div className={styles.CardBody}>
-                <div className={styles.CardTitulo}>Ordem de prisão</div>
-                <div className={styles.CardText}>
-                  Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
-                  Empresária acusada de peculato, associação criminosa,
-                  tráfico de influência e lavagem de dinheiro.
-                  os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+        <div className={styles.Carousel_section}>
+          <Swiper
+            modules={[EffectCards, EffectFade, Pagination, Navigation]}
+            effect={swiperEffect}
+            loop={true}
+            slidesPerView={slidesPerView}
+          >
+            <SwiperSlide>
+              <div className={styles._CardContent}>
+                <div className={styles.CardImage}>
+                  <Image src={comunImage} alt='Imagem de Isabel dos santos' />
                 </div>
-                <Button>
-                  <Link href={'/'} >
-                    Ver
-                  </Link>
-                </Button>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className={styles._CardContent}>
-              <div className={styles.CardImage}>
-                <Image src={comunImage} alt='Imagem de Isabel dos santos' />
-              </div>
-              <div className={styles.CardBody}>
-                <div className={styles.CardTitulo}>Ordem de prisão</div>
-                <div className={styles.CardText}>
-                  Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
-                  Empresária acusada de peculato, associação criminosa,
-                  tráfico de influência e lavagem de dinheiro.
-                  os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+                <div className={styles.CardBody}>
+                  <div className={styles.CardTitulo}>Ordem de prisão</div>
+                  <div className={styles.CardText}>
+                    Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
+                    Empresária acusada de peculato, associação criminosa,
+                    tráfico de influência e lavagem de dinheiro.
+                    os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+                  </div>
+                  <Button>
+                    <Link href={'/'} >
+                      Ver
+                    </Link>
+                  </Button>
                 </div>
-                <Button>
-                  <Link href={'/'} >
-                    Ver
-                  </Link>
-                </Button>
               </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className={styles._CardContent}>
-              <div className={styles.CardImage}>
-                <Image src={comunImage} alt='Imagem de Isabel dos santos' />
-              </div>
-              <div className={styles.CardBody}>
-                <div className={styles.CardTitulo}>Ordem de prisão</div>
-                <div className={styles.CardText}>
-                  Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
-                  Empresária acusada de peculato, associação criminosa,
-                  tráfico de influência e lavagem de dinheiro.
-                  os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles._CardContent}>
+                <div className={styles.CardImage}>
+                  <Image src={comunImage} alt='Imagem de Isabel dos santos' />
                 </div>
-                <Button>
-                  <Link href={'/'} >
-                    Ver
-                  </Link>
-                </Button>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className={styles._CardContent}>
-              <div className={styles.CardImage}>
-                <Image src={comunImage} alt='Imagem de Isabel dos santos' />
-              </div>
-              <div className={styles.CardBody}>
-                <div className={styles.CardTitulo}>Ordem de prisão</div>
-                <div className={styles.CardText}>
-                  Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
-                  Empresária acusada de peculato, associação criminosa,
-                  tráfico de influência e lavagem de dinheiro.
-                  os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+                <div className={styles.CardBody}>
+                  <div className={styles.CardTitulo}>Ordem de prisão</div>
+                  <div className={styles.CardText}>
+                    Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
+                    Empresária acusada de peculato, associação criminosa,
+                    tráfico de influência e lavagem de dinheiro.
+                    os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+                  </div>
+                  <Button>
+                    <Link href={'/'} >
+                      Ver
+                    </Link>
+                  </Button>
                 </div>
-                <Button>
-                  <Link href={'/'} >
-                    Ver
-                  </Link>
-                </Button>
               </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className={styles._CardContent}>
-              <div className={styles.CardImage}>
-                <Image src={comunImage} alt='Imagem de Isabel dos santos' />
-              </div>
-              <div className={styles.CardBody}>
-                <div className={styles.CardTitulo}>Ordem de prisão</div>
-                <div className={styles.CardText}>
-                  Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
-                  Empresária acusada de peculato, associação criminosa,
-                  tráfico de influência e lavagem de dinheiro.
-                  os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles._CardContent}>
+                <div className={styles.CardImage}>
+                  <Image src={comunImage} alt='Imagem de Isabel dos santos' />
                 </div>
-                <Button>
-                  <Link href={'/'} >
-                    Ver
-                  </Link>
-                </Button>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className={styles._CardContent}>
-              <div className={styles.CardImage}>
-                <Image src={comunImage} alt='Imagem de Isabel dos santos' />
-              </div>
-              <div className={styles.CardBody}>
-                <div className={styles.CardTitulo}>Ordem de prisão</div>
-                <div className={styles.CardText}>
-                  Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
-                  Empresária acusada de peculato, associação criminosa,
-                  tráfico de influência e lavagem de dinheiro.
-                  os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+                <div className={styles.CardBody}>
+                  <div className={styles.CardTitulo}>Ordem de prisão</div>
+                  <div className={styles.CardText}>
+                    Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
+                    Empresária acusada de peculato, associação criminosa,
+                    tráfico de influência e lavagem de dinheiro.
+                    os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+                  </div>
+                  <Button>
+                    <Link href={'/'} >
+                      Ver
+                    </Link>
+                  </Button>
                 </div>
-                <Button>
-                  <Link href={'/'} >
-                    Ver
-                  </Link>
-                </Button>
               </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className={styles._CardContent}>
-              <div className={styles.CardImage}>
-                <Image src={comunImage} alt='Imagem de Isabel dos santos' />
-              </div>
-              <div className={styles.CardBody}>
-                <div className={styles.CardTitulo}>Ordem de prisão</div>
-                <div className={styles.CardText}>
-                  Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
-                  Empresária acusada de peculato, associação criminosa,
-                  tráfico de influência e lavagem de dinheiro.
-                  os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles._CardContent}>
+                <div className={styles.CardImage}>
+                  <Image src={comunImage} alt='Imagem de Isabel dos santos' />
                 </div>
-                <Button>
-                  <Link href={'/'} >
-                    Ver
-                  </Link>
-                </Button>
+                <div className={styles.CardBody}>
+                  <div className={styles.CardTitulo}>Ordem de prisão</div>
+                  <div className={styles.CardText}>
+                    Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
+                    Empresária acusada de peculato, associação criminosa,
+                    tráfico de influência e lavagem de dinheiro.
+                    os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+                  </div>
+                  <Button>
+                    <Link href={'/'} >
+                      Ver
+                    </Link>
+                  </Button>
+                </div>
               </div>
-            </div>
-          </SwiperSlide>
-
-        </Swiper>
-
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles._CardContent}>
+                <div className={styles.CardImage}>
+                  <Image src={comunImage} alt='Imagem de Isabel dos santos' />
+                </div>
+                <div className={styles.CardBody}>
+                  <div className={styles.CardTitulo}>Ordem de prisão</div>
+                  <div className={styles.CardText}>
+                    Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
+                    Empresária acusada de peculato, associação criminosa,
+                    tráfico de influência e lavagem de dinheiro.
+                    os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+                  </div>
+                  <Button>
+                    <Link href={'/'} >
+                      Ver
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles._CardContent}>
+                <div className={styles.CardImage}>
+                  <Image src={comunImage} alt='Imagem de Isabel dos santos' />
+                </div>
+                <div className={styles.CardBody}>
+                  <div className={styles.CardTitulo}>Ordem de prisão</div>
+                  <div className={styles.CardText}>
+                    Isabel Dos Santos terá prejudicado Estado angolano em mais de 200 milhões de euros.
+                    Empresária acusada de peculato, associação criminosa,
+                    tráfico de influência e lavagem de dinheiro.
+                    os seus advogados concluem que não existe, até ao momento, qualquer referência à emissão de um mandado.
+                  </div>
+                  <Button>
+                    <Link href={'/'} >
+                      Ver
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </section>
     </main>
   )
