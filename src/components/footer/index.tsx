678d4ceed1fa255d90c7a715ddfb08ea0989e438
Anderson Pedro <113034804+ApDev0@users.noreply.github.com>
2024-01-16 07:56:46 +0000

    consu 2K24 V1.1.3

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import Image from 'next/image'
-import Logo from '../../../public/assets/footer_angola.png'
+import mirexLogo from '../../../public/assets/footer_angola.png'
+import Logo from '../../../public/assets/footer-logo.png'
+import Link from 'next/link'
+
 function Footer() {
   return (
     <section className={styles.FooterContent}>
       <div className={styles.LogoFooter}>
-        <h1>MIREX</h1>
+        <Image src={Logo} alt='Logo do governo de angola' className={styles.logoImg} />
+        <hr className={styles.line} />
+        <Image src={mirexLogo} alt='Logo do mirex angola' className={styles.mirexlogo} />
       </div>
+      <div className={styles.textFooter}>
+        <p>© 2024 CONSULADO GERAL DA REPÚBLICA DE ANGOLA NO PORTO</p>
+        <p>Designed By <Link href={'www.apdev.com'}>DevSembo</Link></p>
+      </div>
+
     </section>
   )
 }
